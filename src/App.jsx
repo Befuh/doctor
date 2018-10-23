613a3076fc1aca29ab83645f6ea32801ac577412
Roland Awemo <awemo@publicplan.de>
2018-10-23 22:52:37 +0200

    :lipstick: Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import configureStore from './store/configureStore';
 import SearchForm from './components/patients/SearchForm';
+
+import './App.scss'
+import 'normalize.css/normalize.css'
 
 const store = configureStore();
 
 const App = (
   <Provider store={store}>
-    <SearchForm/>
+    <Fragment>
+      <Header/>
+      <SearchForm/>
+      <Footer/>
+    </Fragment>
   </Provider>
 );
 
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: path.join(__dirname, 'src/app.jsx'),
+  entry: path.join(__dirname, 'src/App.jsx'),
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
