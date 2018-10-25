942b25a4437a4658da1b8b53c180df2978e3fbf6
Roland Awemo <awemo@publicplan.de>
2018-10-25 22:29:43 +0200

    :lipstick: Add theming

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
 
 const Header = () => (
-  <div>Header</div>
+  <Navbar bg="primary" expand="lg">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  </Navbar>
 );
 
 export default Header;
