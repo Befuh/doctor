111718dd37647c6cb2f801150a4421130ea65ba1
Roland Awemo <awemo@publicplan.de>
2018-11-27 22:48:19 +0100

    :lipstick: redesign header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,41 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Logo from '../../images/Logo.png';
-import { Collapse, Container, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import {
+  Nav,
+  Navbar,
+  Container,
+  NavbarBrand,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  UncontrolledDropdown
+} from 'reactstrap';
 
 import './Header.scss';
 
-class Header extends Component {
-  state = {
-    isOpen: false
-  };
-
-  toggle = event => {
-    event.preventDefault();
-    this.setState({ isOpen: !this.state.isOpen });
-  };
-
-  render () {
-    return (
-      <Navbar expand='md' color='light' light className='header'>
-        <Container className='header-container'>
-          <NavbarBrand href='#home'>
-            <img src={Logo} alt='Befu'/>
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle}/>
-          <Collapse isOpen={this.state.isOpen} navbar className='push-right'>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href='#' className='active navigation-link'>Patients</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href='#' className='navigation-link'>Hospitals</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-}
+const Header = () => (
+  <Navbar expand='md' className='header'>
+    <Container className='header-container'>
+      <NavbarBrand href='#home'>
+        <img src={Logo} alt='Befu'/>
+      </NavbarBrand>
+      <Nav navbar className='align-content-end'>
+        <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav caret className='navigation-link'>
+            Hi, Dr. Alex
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+              Account
+            </DropdownItem>
+            <DropdownItem divider/>
+            <DropdownItem>
+              Logout
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+    </Container>
+  </Navbar>
+);
 
 export default Header;
