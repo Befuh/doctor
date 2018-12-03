3fd6b890b85992f8eaafe7d403080680f18f56e4
Roland Awemo <awemo@publicplan.de>
2018-12-03 22:26:41 +0100

    :sparkles: connect patients list to search

--- src/components/patients/List.jsx
+++ src/components/patients/List.jsx
@@ -1,10 +1,17 @@
 import { Table } from 'reactstrap';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import React, { PureComponent } from 'react';
 import { FaEye, FaPen } from 'react-icons/fa';
 
 import './List.scss';
 
 class List extends PureComponent {
+  sexes = {
+    male: 'Male',
+    female: 'Female'
+  };
+
   render () {
     return (
       <Table striped borderless hover className='patients-list'>
@@ -18,50 +25,33 @@ class List extends PureComponent {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope='row'>6tdaf1</th>
-            <td>Cristiano</td>
-            <td>Ronaldo</td>
-            <td>Male</td>
-            <td className='actions'>
-              <a href='#' className='btn btn-outline-primary btn-sm'><FaEye/> View</a>
-              <a href='#' className='btn btn-outline-primary btn-sm'><FaPen/> Edit</a>
-            </td>
-          </tr>
-          <tr>
-            <th scope='row'>6tdaf1</th>
-            <td>Cristiano</td>
-            <td>Ronaldo</td>
-            <td>Male</td>
-            <td className='actions'>
-              <a href='#' className='btn btn-outline-primary btn-sm'><FaEye/> View</a>
-              <a href='#' className='btn btn-outline-primary btn-sm'><FaPen/> Edit</a>
-            </td>
-          </tr>
-          <tr>
-            <th scope='row'>6tdaf1</th>
-            <td>Cristiano</td>
-            <td>Ronaldo</td>
-            <td>Male</td>
-            <td className='actions'>
-              <a href='#' className='btn btn-outline-primary btn-sm'><FaEye/> View</a>
-              <a href='#' className='btn btn-outline-primary btn-sm'><FaPen/> Edit</a>
-            </td>
-          </tr>
-          <tr>
-            <th scope='row'>6tdaf1</th>
-            <td>Cristiano</td>
-            <td>Ronaldo</td>
-            <td>Male</td>
-            <td className='actions'>
-              <a href='#' className='btn btn-outline-primary btn-sm'><FaEye/> View</a>
-              <a href='#' className='btn btn-outline-primary btn-sm'><FaPen/> Edit</a>
-            </td>
-          </tr>
+          {
+            this.props.patients &&
+            this.props.patients.map((patient, index) => (
+              <tr key={index}>
+                <th scope='row'>{patient.identifier}</th>
+                <td>{patient.user.data.first_name}</td>
+                <td>{patient.user.data.last_name}</td>
+                <td>{this.sexes[patient.user.data.sex]}</td>
+                <td className='actions'>
+                  <a href='#' className='btn btn-outline-primary btn-sm'><FaEye/> View</a>
+                  <a href='#' className='btn btn-outline-primary btn-sm'><FaPen/> Edit</a>
+                </td>
+              </tr>
+            ))
+          }
         </tbody>
       </Table>
     )
   }
 }
 
-export default List;
+List.propTypes = {
+  patients: PropTypes.array
+};
+
+const mapStateToProps = ({ patients }) => ({
+  patients
+});
+
+export default connect(mapStateToProps)(List);
