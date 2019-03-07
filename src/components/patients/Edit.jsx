2b28ee451a77fd4e9c2d5c6c88b11ca722584b5a
Roland Awemo <awemo@publicplan.de>
2019-03-07 22:10:18 +0100

    ✨ add patient edit form

--- src/components/patients/Edit.jsx
+++ src/components/patients/Edit.jsx
@@ -27,19 +27,37 @@ export class Edit extends Component {
         <Form>
           <FormGroup>
             <Label for='first_name'>First Name</Label>
-            <Input type='text' id="first_name" placeholder='First Name' value={firstName}/>
+            <Input
+              type='text'
+              id="first_name"
+              placeholder='First Name'
+              value={firstName}
+              onChange={this.changeValue('first_name')}
+            />
           </FormGroup>
           <FormGroup>
             <Label for='last_name'>Last Name</Label>
-            <Input type='text' id="last_name" placeholder='Last Name' value={lastName}/>
+            <Input
+              type='text'
+              id="last_name"
+              placeholder='Last Name'
+              value={lastName}
+              onChange={this.changeValue('last_name')}
+            />
           </FormGroup>
           <FormGroup>
             <Label for='date_of_birth'>Date of Birth</Label>
-            <Input type='date' id="date_of_birth" placeholder='Date of Birth' value={dateOfBirth}/>
+            <Input
+              type='date'
+              id="date_of_birth"
+              placeholder='Date of Birth'
+              value={dateOfBirth}
+              onChange={this.changeValue('date_of_birth')}
+            />
           </FormGroup>
           <FormGroup>
             <Label for='sex'>Sex</Label>
-            <Input type='select' id="sex" placeholder='Sex' value={sex}>
+            <Input type='select' id="sex" placeholder='Sex' value={sex} onChange={this.changeValue('sex')}>
               <option>female</option>
               <option>male</option>
             </Input>
@@ -51,6 +69,13 @@ export class Edit extends Component {
       </Fragment>
     )
   }
+
+  changeValue = (key) => (event) => {
+    event.preventDefault();
+    let patient = this.state.patient;
+    patient.user.data[key] = event.target.value;
+    this.setState(() => ({ patient }));
+  };
 }
 
 const mapStateToProps = () => ({});
--- tests/src/components/patients/Edit.test.jsx
+++ tests/src/components/patients/Edit.test.jsx
@@ -20,8 +20,25 @@ beforeEach(() => {
   };
   edit = shallow(<Edit patient={patient}/>)
 });
+
 describe('Edit patients', () => {
   it('renders correctly', () => {
     expect(edit).toMatchSnapshot();
   });
+
+  [
+    { id: 'first_name', value: 'Cristiana' },
+    { id: 'last_name', value: 'Ronaldinha' },
+    { id: 'date_of_birth', value: '1985-07-09' },
+    { id: 'sex', value: 'female' }
+  ].forEach(({ id, value }) => {
+    it(`changes the input ${id}`, () => {
+      const preventDefault = jest.fn();
+
+      edit.find(`#${id}`).simulate('change', { preventDefault, target: { value } });
+
+      expect(preventDefault).toHaveBeenCalled();
+      expect(edit.state().patient.user.data[id]).toBe(value);
+    });
+  })
 });
